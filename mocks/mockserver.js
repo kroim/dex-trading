88cfcf10cd465c1f2376fe197b574f2865f931b0
kroim <wonderfulplayer@outlook.com>
2020-11-20 18:29:08 +0800

    Updated mock server origin

--- mocks/mockserver.js
+++ mocks/mockserver.js
@@ -283,14 +283,9 @@ const mockserver = {
         this.headers = prepareWatchedHeaders();
     },
     handle: function (req, res) {
-        // Website you wish to allow to connect
-        res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-        // Request methods you wish to allow
-        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-        // Request headers you wish to allow
-        res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-        // Set to true if you need the website to include cookies in the requests sent
-        // to the API (e.g. in case you use sessions)
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+        res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,Content-type,Accept');
         res.setHeader('Access-Control-Allow-Credentials', true);
         getBody(req, async function (body) {
             req.body = body;
